065bbde9d4302a033667349b2677d53b244efba0
dean <dean@deanproxy.com>
2016-09-15 03:05:22 -0400

    fix mobile layout

--- public/javascripts/react/layout.js
+++ public/javascripts/react/layout.js
@@ -9,24 +9,6 @@ class Layout extends React.Component {
     }
     return (
       <div className="layout">
-        <nav className="nav navbar-default">
-          <div className="container-fluid navbar-left">
-            <div className="navbar-header">
-              <Link to="/" className="navbar-brand">
-                <em>dean</em>proxy
-              </Link>
-            </div>
-          </div>
-          <div className="collapse navbar-collapse navbar-right">
-            <ul className="nav navbar-nav">
-              <li><Link to="code">code</Link></li>
-              <li><Link to="music">music</Link></li>
-              <li><Link to="about">about</Link></li>
-              <li><Link to="contact">contact</Link></li>
-              {logoutLink}
-            </ul>
-          </div>
-        </nav>
         <div className="main-content">
           {this.props.children}
         </div>
